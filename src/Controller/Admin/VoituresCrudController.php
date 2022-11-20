ed064cf0be512f0496baa32d15c388e02c182521
Gb16702 <delgeoffrey1@gmail.com>
2022-11-20 22:51:19 +0100

    Ajustements

--- src/Controller/Admin/VoituresCrudController.php
+++ src/Controller/Admin/VoituresCrudController.php
@@ -6,14 +6,18 @@ use App\Entity\Voitures;
 use App\Entity\ImagesVoitures;
 use App\Form\ImagesVoituresType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use PhpParser\Node\Stmt\Label;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -31,9 +35,26 @@ class VoituresCrudController extends AbstractCrudController
             TextField::new('nom'),
             TextField::new('titre'),
             TextEditorField::new('description'),
-            TextField::new('carburant'),
-            MoneyField::new('prix') -> setCurrency("EUR"),
+            ChoiceField::new("carburant")->setChoices([
+                'Essence' => 'Essence',
+                'Diesel' => 'Diesel',
+                'Electrique' => 'Electrique',
+            ]),
+            ChoiceField::new("cylinder")->setChoices([
+                '1 litre' => '1',
+                '2 litres' => '2',
+                '3 litres' => '3',
+            ]),
+            ChoiceField::new("cylinder")->setChoices([
+               'Avant' => 'Avant',
+               'Arrière' => "Arrière"
+            ]),
+            IntegerField::new('owner'),
+            IntegerField::new('puissance'),
             IntegerField::new('km'),
+            IntegerField::new('prix'),
+            DateTimeField::new('Year')->setFormat('yyyy.MM.dd G')
+            ->setLabel('Date de mise en circulation'),
             AssociationField::new('marque')
             ->setFormTypeOptions((['by_reference' => true])),
 
@@ -41,6 +62,7 @@ class VoituresCrudController extends AbstractCrudController
             setEntryType(ImagesVoituresType::class)
             -> onlyOnForms(),
             SlugField:: new ('slug') ->setTargetFieldName('nom'),
+
         ];
     }
 
